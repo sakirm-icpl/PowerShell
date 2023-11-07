3315e39dee0bc8ef85f7b5e825b90ef73f0fd876
Sakir Malek <sakirm@infopercept.com>
2023-11-07 10:14:17 +0530

    Delete Directory.ps1

--- Directory.ps1
+++
@@ -1,6 +0,0 @@
-﻿New-Item -Path 'C:\NewPowerShellFolder' -ItemType Directory # For Create Folder
-Copy-Item 'C:\NewPowerShellFolder' 'C:\Powershell' # For Copy Folder
-Move-Item C:\NewPowerShellFolder C:\Powershell # For Move Folder
-Rename-Item 'C:\Powershell\NewPowerShellFolder' # For Rename Folder
-Remove-Item 'C:\Powershell\NewPowerShellFolder' # For Remove Folder
-Test-Path 'C:\Powershell\NewFolder' # For Check Folder
